833b0c9c3b12325c0869ae9ea73daddefcb959da
Farid Ardiansyah <faridardiansyah061@gmail.com>
2024-08-17 10:29:07 +0700

    Add files via upload

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,102 @@
+import Link from 'next/link'
+import MaxWidthWrapper from './MaxWidthWrapper'
+import { buttonVariants } from './ui/button'
+import {
+  LoginLink,
+  RegisterLink,
+  getKindeServerSession,
+} from '@kinde-oss/kinde-auth-nextjs/server'
+import { ArrowRight, Github } from 'lucide-react'
+import UserAccountNav from './UserAccountNav'
+import MobileNav from './MobileNav'
+
+const Navbar = () => {
+  const { getUser } = getKindeServerSession()
+  const user = getUser()
+
+  return (
+    <nav className='sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
+      <MaxWidthWrapper>
+        <div className='flex h-14 items-center justify-between border-b border-zinc-200'>
+          <Link
+            href='/'
+            className='flex z-40 font-semibold'>
+            <span>ReadifyAI</span>
+          </Link>
+
+          <MobileNav isAuth={!!user} />
+
+          <div className='hidden items-center space-x-4 sm:flex'>
+            {!user ? (
+              <>
+                <Link
+                  target='_blank'
+                  href='https://github.com/rohitt-gupta/chatpdf-saas'
+                  className={buttonVariants({
+                    variant: 'ghost',
+                    size: 'sm',
+                  })}>
+                  <Github className='w-5 h-5' />
+                </Link>
+                <Link
+                  href='/pricing'
+                  className={buttonVariants({
+                    variant: 'ghost',
+                    size: 'sm',
+                  })}>
+                  Pricing
+                </Link>
+                <LoginLink
+                  className={buttonVariants({
+                    variant: 'ghost',
+                    size: 'sm',
+                  })}>
+                  Sign in
+                </LoginLink>
+                <RegisterLink
+                  className={buttonVariants({
+                    size: 'sm',
+                  })}>
+                  Get started{' '}
+                  <ArrowRight className='ml-1.5 h-5 w-5' />
+                </RegisterLink>
+              </>
+            ) : (
+              <>
+                <Link
+                  target='_blank'
+                  href='https://github.com/rohitt-gupta/chatpdf-saas'
+                  className={buttonVariants({
+                    variant: 'ghost',
+                    size: 'sm',
+                  })}>
+                  <Github className='w-5 h-5' />
+                </Link>
+                <Link
+                  href='/dashboard'
+                  className={buttonVariants({
+                    variant: 'ghost',
+                    size: 'sm',
+                  })}>
+                  Dashboard
+                </Link>
+
+                <UserAccountNav
+                  name={
+                    !user.given_name || !user.family_name
+                      ? 'Your Account'
+                      : `${user.given_name} ${user.family_name}`
+                  }
+                  email={user.email ?? ''}
+                  imageUrl={user.picture ?? ''}
+                />
+              </>
+            )}
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  )
+}
+
+export default Navbar
